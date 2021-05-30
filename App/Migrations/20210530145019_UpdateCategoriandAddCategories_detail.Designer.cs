f910c4b5a11c3d20f24d7a683d3c188366b29637
thanh nguyen <01658532049thanh@github.com>
2021-05-30 22:01:22 +0700

    update Category and category_detail

--- App/Migrations/20210530145019_UpdateCategoriandAddCategories_detail.Designer.cs
+++ App/Migrations/20210530145019_UpdateCategoriandAddCategories_detail.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using App.Infrastructures.Dbcontexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace App.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210530145019_UpdateCategoriandAddCategories_detail")]
+    partial class UpdateCategoriandAddCategories_detail
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ App/Models/Entities/Categories_detail .cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace App.Models.Entities
+{
+    public class Categories_detail
+    {
+        [MaxLength(10,ErrorMessage ="")]
+        public Category Parent { get; set; }
+        public Lang langiId{ get; set; }
+        [Column("Name")]
+        public string Name { get; set; }
+        [Column("Description")]
+        public string Description { get; set; }
+    }
+}
